710cc5d9d47b773f0bafa1bff1b5f0a5223eaea9
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-02-25 09:25:03 +0000

    heroku change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = process.env.PORT || 3000;
+var port = process.ENV.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
