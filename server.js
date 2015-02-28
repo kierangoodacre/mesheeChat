250c1376a24a69bd2aab8f98a036bf49703e6177
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-28 17:26:35 +0000

    Added server compatibility for ipv6

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app).listen(3000, '::');
+var io = require('socket.io')(server);
 var path = require('path');
 var express = require('express');
 var people = {};
 var socket = io;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/public/js'))
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
-socket.on("connection", function (client) { 
+socket.on("connection", function (client) {
     client.on("join", function(user){
         people[client.id] = user;
-        console.log(user)
         client.emit("update", "You have connected to the server.");
         client.broadcast.emit("update", people[client.id] + " has joined the server.")
         // socket.sockets.emit("update-people", people);
@@ -39,6 +39,6 @@ socket.sockets.on('connection', function(client){
     });
 });
 
-http.listen(process.env.PORT || 3001, function(){
-  console.log('listening on *:3001');
-});
+// server.listen(process.env.PORT || 3001, function(){
+//   console.log('listening on *:3001');
+// });
