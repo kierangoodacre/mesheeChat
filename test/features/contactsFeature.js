6e35b0f8e2982b2c65f0420e98db1340641211de
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-01 20:32:04 +0000

    Created test to check contacts database is being read

--- test/features/contactsFeature.js
+++ test/features/contactsFeature.js
@@ -1,12 +1,12 @@
 describe("Contacts", function() {
 
   before(function() {
-    casper.start('http://localhost:3001/');
+    casper.start('http://localhost:3001/contacts');
   });
 
   it('should display a list of contacts', function() {
     casper.then(function() {
-      expect('.contacts').to.include.text('Clint');
+      expect('#contact-name').to.contain.text('clint');
     });
   });
 
