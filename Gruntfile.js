cf73bd86dcf6264b00e553747f591586e7100286
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-01 15:42:00 +0000

    Editing gruntfile to make tests work through grunt. However, having issue getting socket tests to pass

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,39 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     mocha_casperjs: {
       options: {
+        timeout: 5000,
+        color: false
       },
       files: {
-        src: ['test/**/*']
+        src: ['test/features/**/*.js']
+      }
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt',
+          quiet: false,
+          clearRequireCache: false
+        },
+        src: ['test/spec/**/*.js']
+      }
+    },
+    express: {
+      server: {
+        options: {
+          script: './server.js',
+          hostname: 'localhost',
+          port: 3001
+        }
       }
     }
   })
-  grunt.loadNpmTasks('grunt-mocha-casperjs');
 
-  grunt.registerTask('default', ['mocha_casperjs']);
+  grunt.loadNpmTasks('grunt-mocha-casperjs');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-express');
+
+  grunt.registerTask('default', ['express', 'mocha_casperjs', 'mochaTest']);
+
 };
