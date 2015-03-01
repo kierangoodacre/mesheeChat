97e4a70fe78c1fee9b50f027bb20921693f52b38
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-01 20:35:11 +0000

    Created contacts db and reading this in models

---
+++ app/models/contacts.js
@@ -0,0 +1,7 @@
+var mongo = require('mongodb');
+var monk = require('monk');
+var contacts = monk('localhost:27017/contacts');
+
+exports.mongo = mongo;
+exports.monk = monk;
+exports.db = contacts;
