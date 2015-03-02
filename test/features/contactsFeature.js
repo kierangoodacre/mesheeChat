612f88047b9008cfa8b88de2981f6348f2e0ed48
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-02 12:11:36 +0000

    Commented out contacts until next sprint

--- test/features/contactsFeature.js
+++ test/features/contactsFeature.js
@@ -1,13 +1,28 @@
-describe("Contacts", function() {
+// describe("Contacts", function() {
 
-  before(function() {
-    casper.start('http://localhost:3001/contacts');
-  });
+//   it('should display a list of contacts', function() {
+//     casper.start('http://localhost:3001/contacts');
+//     casper.then(function() {
+//       expect('#contact-name').to.contain.text('clint');
+//     });
+//   });
 
-  it('should display a list of contacts', function() {
-    casper.then(function() {
-      expect('#contact-name').to.contain.text('clint');
-    });
-  });
+//   it('should enable creation of contacts', function() {
+//     casper.start('http://localhost:3001/newcontact');
+//       casper.then(function() {
 
-});
+//         this.fill('form[id="add-user"]',{
+//           user: 'Jake',
+//           ip: ''
+//         }, true);
+
+//         this.click('input[id="join"]');
+//           setTimeout(function() {
+//             expect('body').to.contain.text('Clint');
+//           }
+//         }, 1000);
+//       });
+//     });
+//   });
+
+// });
