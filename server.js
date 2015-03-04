9401c5bf33b30d5e00225cb496f34c708f419147
Oliver <o.lucas@live.com>
2015-03-04 13:10:54 +0000

    server cleaned up

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var app = require('express')();
 var server = require('http').createServer(app).listen(3001, '::');
-var io = require('socket.io')(server);
 var path = require('path');
 var express = require('express');
 var people = {};
-var socket = io;
+
+// var sockets = require('./app/src/sockets/sockets.js')
 
 // require('./app/models/contacts.js');
 
@@ -19,25 +19,26 @@ app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/css', express.static(__dirname + '/public/css'));
 
 
-socket.on("connection", function (client) {
-  client.on("join", function(user){
-    people[client.id] = user;
-    client.emit("update", "You have connected to the server.");
-    client.broadcast.emit("update", people[client.id] + " has joined the server.")
-    console.log(people);
-  });
+require('./app/src/sockets/sockets.js')(server);
+// socket.on("connection", function (client) {
+//   client.on("join", function(user){
+//     people[client.id] = user;
+//     client.emit("update", "You have connected to the server.");
+//     client.broadcast.emit("update", people[client.id] + " has joined the server.")
+//     console.log(people);
+//   });
 
-  client.on("message", function(msg){
-    socket.sockets.emit("chat", people[client.id], msg);
-  });
+//   client.on("message", function(msg){
+//     socket.sockets.emit("chat", people[client.id], msg);
+//   });
 
-  client.on("disconnect", function(){
-    socket.sockets.emit("update-disconnect", people[client.id] + " has left the server.");
-    delete people[client.id];
-    socket.sockets.emit("update-people", people);
-  });
+//   client.on("disconnect", function(){
+//     socket.sockets.emit("update-disconnect", people[client.id] + " has left the server.");
+//     delete people[client.id];
+//     socket.sockets.emit("update-people", people);
+//   });
 
-  client.on('user-list', function(user){
-    client.emit("logged-in-users", people);
- });
-});
+//   client.on('user-list', function(user){
+//     client.emit("logged-in-users", people);
+//  });
+// });
