fab203e68b4d589dc02dc28638ce03e9d33b5f0e
Oliver <o.lucas@live.com>
2015-03-04 13:11:59 +0000

    deleted a few uneccessary bits of code

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var path = require('path');
 var express = require('express');
 var people = {};
 
-// var sockets = require('./app/src/sockets/sockets.js')
 
 // require('./app/models/contacts.js');
 
@@ -20,25 +19,3 @@ app.use('/css', express.static(__dirname + '/public/css'));
 
 
 require('./app/src/sockets/sockets.js')(server);
-// socket.on("connection", function (client) {
-//   client.on("join", function(user){
-//     people[client.id] = user;
-//     client.emit("update", "You have connected to the server.");
-//     client.broadcast.emit("update", people[client.id] + " has joined the server.")
-//     console.log(people);
-//   });
-
-//   client.on("message", function(msg){
-//     socket.sockets.emit("chat", people[client.id], msg);
-//   });
-
-//   client.on("disconnect", function(){
-//     socket.sockets.emit("update-disconnect", people[client.id] + " has left the server.");
-//     delete people[client.id];
-//     socket.sockets.emit("update-people", people);
-//   });
-
-//   client.on('user-list', function(user){
-//     client.emit("logged-in-users", people);
-//  });
-// });
