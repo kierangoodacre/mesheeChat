84e0b401994b297f637af143aa2b0b7c6b9fa4af
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-04 16:01:25 +0000

    Updated server.js and removed sockets from server

--- server.js
+++ server.js
@@ -2,20 +2,12 @@ var app = require('express')();
 var server = require('http').createServer(app).listen(3001, '::');
 var path = require('path');
 var express = require('express');
-var people = {};
-
-
-// require('./app/models/contacts.js');
 
 require('./app/controllers/index.js')(app);
-// require('./app/controllers/database.js')(app);
-// require('./app/controllers/contacts.js')(app);
-// require('./app/controllers/contacts/newContact.js')(app);
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/css', express.static(__dirname + '/public/css'));
-
 
 require('./app/src/sockets/sockets.js')(server);
