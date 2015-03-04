6b79ab7206ad7ed2d0ef875985f95e8fc75a80b2
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-04 11:11:12 +0000

    Created tests for index

--- test/features/indexFeature.js
+++ test/features/indexFeature.js
@@ -14,30 +14,52 @@ describe('Homepage', function() {
     client.end(done)
   });
 
-  function inputName() {
+  function inputName(name) {
     client
       .url('http://localhost:3001')
-      .setValue('#name', 'Clint')
+      .setValue('#name', name)
       .click('#join')
     return client;
   };
 
-  it('should tell user when it has connected', function(done) {
-    inputName()
-      .getText('#msgs', function(err, text){
-        expect(text).to.contain('You have connected to the server.')
-      })
-      .call(done);
-  });
-
   it('should let a user know when a message has been sent', function(done){
-   inputName()
+   inputName('Clint')
     .setValue('#msg', 'Hi from Clint')
     .click('#send')
     .getText('#msgs', function(err, text){
       expect(text).to.contain('Hi from Clint')
     })
     .call(done);
+  });
+
+  it('should let a user know who is talking', function(done) {
+    inputName('Clint')
+    .setValue('#msg', 'Hi from Clint')
+    .click('#send')
+    .getText('#msgs', function(err, text){
+      expect(text).to.contain('Clint says:')
+    })
+    .call(done);
+  });
+
+  it('should tell user when it has connected', function(done) {
+    inputName('Clint')
+      .getText('#msgs', function(err, text){
+        expect(text).to.contain('You have connected to the server.')
+      })
+      .call(done);
+  });
+
+  it('should let a user know when someone has disconnected from the server', function(done){
+    inputName('Clint')
+      .newWindow('http://localhost:3001', 'Chat 2')
+      .setValue('#name', 'Jake')
+      .click('#join')
+      .switchTab()
+      .getText('#msgs', function(err, text){
+        expect(text).to.contain('Jake has left the server')
+      })
+      .call(done);
   });
 
 });
