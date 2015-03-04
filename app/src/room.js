070ee384e108747169e843ae7b652734d3764ce5
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-04 11:16:36 +0000

    Created rooms class

---
+++ app/src/room.js
@@ -0,0 +1,18 @@
+var Room = function(name,id,owner) {
+
+  this.name = name;
+  this.id = id;
+  this.owner = owner;
+  this.people = [];
+  this.status = 'available';
+};
+
+Room.prototype.addPerson = function(personID) {
+
+  if(this.status === 'available') {
+    this.people.push(personID);
+  }
+
+};
+
+module.exports = Room;
